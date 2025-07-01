c5f03689310e98dd504a00e9e011e61e8c6117d5
GeonhaPark <geonhab504@gmail.com>
2025-07-01 12:00:08 +0900

    step 01 code update

--- src/main_cpu.cpp
+++ src/main_cpu.cpp
@@ -30,85 +30,35 @@ int main(int argc, char *argv[])
     const std::string label_path = argv[3];
 
     /* Load model */
-    std::unique_ptr<tflite::FlatBufferModel> model =
-        tflite::FlatBufferModel::BuildFromFile(model_path.c_str());
-    if (!model)
-    {
-        std::cerr << "Failed to load model" << std::endl;
-        return 1;
-    }
+
 
     /* Build interpreter */
-    tflite::ops::builtin::BuiltinOpResolver resolver;
-    tflite::InterpreterBuilder builder(*model, resolver);
-    std::unique_ptr<tflite::Interpreter> interpreter;
-    builder(&interpreter);
+
 
     /* Apply XNNPACK delegate */
-    TfLiteXNNPackDelegateOptions xnnpack_opts = TfLiteXNNPackDelegateOptionsDefault();
-    TfLiteDelegate *xnn_delegate = TfLiteXNNPackDelegateCreate(&xnnpack_opts);
-    bool delegate_applied = false;
-    if (interpreter->ModifyGraphWithDelegate(xnn_delegate) == kTfLiteOk)
-    {
-        delegate_applied = true;
-    }
-    else
-    {
-        std::cerr << "Failed to Apply XNNPACK Delegate" << std::endl;
-    }
+
 
     /* Allocate Tensor */
-    if (!interpreter || interpreter->AllocateTensors() != kTfLiteOk)
-    {
-        std::cerr << "Failed to initialize interpreter" << std::endl;
-        return 1;
-    }
 
-    util::print_model_summary(interpreter.get(), delegate_applied);
 
     /* Load input image */
-    cv::Mat origin_image = cv::imread(image_path);
-    if (origin_image.empty())
-        throw std::runtime_error("Failed to load image: " + image_path);
+
 
     /* Preprocessing */
     // Get input tensor info
-    TfLiteTensor *input_tensor = interpreter->input_tensor(0);
-    int input_height = input_tensor->dims->data[1];
-    int input_width = input_tensor->dims->data[2];
-    int input_channels = input_tensor->dims->data[3];
 
-    std::cout << "\n[INFO] Input shape  : ";
-    util::print_tensor_shape(input_tensor);
-    std::cout << std::endl;
 
     // Preprocess input data
-    cv::Mat preprocessed_image = util::preprocess_image(origin_image, input_height, input_width);
 
     // Copy HWC float32 cv::Mat to TFLite input tensor
-    float *input_tensor_buffer = interpreter->typed_input_tensor<float>(0);
-    std::memcpy(input_tensor_buffer, preprocessed_image.ptr<float>(),
-                preprocessed_image.total() * preprocessed_image.elemSize());
+
 
     /* Inference */
-    if (interpreter->Invoke() != kTfLiteOk)
-    {
-        std::cerr << "Failed to invoke interpreter" << std::endl;
-        return 1;
-    }
+
 
     /* PostProcessing */
     // Get output tensor
-    TfLiteTensor *output_tensor = interpreter->output_tensor(0);
-    std::cout << "[INFO] Output shape : ";
-    util::print_tensor_shape(output_tensor);
-    std::cout << std::endl;
 
-    float *logits = interpreter->typed_output_tensor<float>(0);
-    int num_classes = output_tensor->dims->data[1];
-
-    std::vector<float> probs(num_classes);
-    util::softmax(logits, probs, num_classes);
 
     /* Print Results */
     // Load class label mapping
@@ -124,9 +74,6 @@ int main(int argc, char *argv[])
     }
 
     /* Deallocate delegate */
-    if (xnn_delegate)
-    {
-        TfLiteXNNPackDelegateDelete(xnn_delegate);
-    }
+
     return 0;
 }
