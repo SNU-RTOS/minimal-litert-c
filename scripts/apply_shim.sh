a1e4858173c7628d9cce5d38fd2b871abeb5b731
NamcheolLee <nclee@redwood.snu.ac.kr>
2025-07-18 14:01:29 +0900

    Renamed files

--- scripts/apply_shim.sh
+++ scripts/apply_shim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Changing symbolic links
-sudo ln -sf "$(pwd)/output/libOpenCL_shim_profiling.so" /usr/lib/libOpenCL_shim_profiling.so
-sudo ln -sf /usr/lib/libOpenCL_shim_profiling.so /usr/lib/libOpenCL.so
+sudo ln -sf "$(pwd)/output/libOpenCL_profiling_shim.so" /usr/lib/libOpenCL_profiling_shim.so
+sudo ln -sf /usr/lib/libOpenCL_profiling_shim.so /usr/lib/libOpenCL.so
